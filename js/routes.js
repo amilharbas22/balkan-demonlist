a3d06abde8d42b14d51f16f2fc9d14a9c6a7e3b7
real-danish <danishuseinspahic@yahoo.com>
2024-10-01 20:47:14 +0200

    Update routes.js

--- js/routes.js
+++ js/routes.js
@@ -1,6 +1,6 @@
 import List from './pages/List.js';
 import Leaderboard from './pages/Leaderboard.js';
-import Roulette from './pages/Roulette.js';
+import Challenge_List from './pages/Roulette.js';
 
 export default [
     { path: '/', component: List },
